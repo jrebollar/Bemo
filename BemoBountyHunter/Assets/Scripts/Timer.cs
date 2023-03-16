47601c10676bdad858c40a2cde4295db2ba55310
Joshua Barrientos <56335048+crimlast@users.noreply.github.com>
2023-03-16 16:10:37 -0700

    last scene addition
    added a timer and some materials that might be useful for the ending of the game.

---
+++ BemoBountyHunter/Assets/Scripts/Timer.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+    private float curTime;
+    private bool isTimerRunning;
+
+    public TMP_Text textObject;
+    public float startTimeSeconds;
+
+    [SerializeField] GameObject secretEnding;
+    private void Start()
+    {
+        curTime = startTimeSeconds;
+        isTimerRunning = true;
+    }
+
+    private void Update()
+    {
+        if(isTimerRunning)
+        {
+            if (curTime > 0)
+            {
+                // Update the timer and display it
+                curTime -= Time.deltaTime;
+                DisplayTime();
+            }
+            else
+            {
+                // Keep timer at 0, and stop it
+                curTime = 0;
+                isTimerRunning = false;
+            }
+        }
+    }
+
+    // Display time left on UI
+    private void DisplayTime()
+    {
+        // Only display time left based on curTime if it is above or equal to zero
+        if(curTime >= 0)
+        {
+            // Caculate and format time to display it
+            int minutes = Mathf.FloorToInt(curTime / 60);
+            int seconds = Mathf.FloorToInt(curTime % 60);
+            textObject.text = string.Format("{0}:{1:00}", minutes, seconds);
+        }
+        else
+        {
+            textObject.text = "0:00";
+            DestroyOptions();
+            ActivateSecretEnding();
+        }
+    }
+    public bool timeRunning()
+    {
+        return isTimerRunning;
+    }
+    public float GetCurrentTime()
+    {
+        return curTime;
+    }
+    private void DestroyOptions()
+    {
+        GameObject obj = GameObject.Find("Options");
+        Destroy(obj);
+    }
+
+    private void ActivateSecretEnding(){
+        secretEnding.SetActive(true);
+    }
+}
+
