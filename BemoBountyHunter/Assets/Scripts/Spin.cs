a96cbb1bfc27e3d41553e38c8cfff2386d876d39
rlee56800 <78502232+rlee56800@users.noreply.github.com>
2023-03-17 14:28:14 -0700

    Jellyfish glow up (pun intended)

--- BemoBountyHunter/Assets/Scripts/Spin.cs
+++ BemoBountyHunter/Assets/Scripts/Spin.cs
@@ -4,10 +4,19 @@ using UnityEngine;
 
 public class Spin : MonoBehaviour
 {
-    
+    float originalY;
+    public float floatStrength = 1;
+
+    void Start()
+    {
+        this.originalY = this.transform.position.y;
+    }
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(new Vector3(0f, 10f, 0f) * Time.deltaTime);
+        transform.position = new Vector3(transform.position.x,
+        originalY + ((float)Mathf.Sin(Time.time) * floatStrength),
+        transform.position.z);
+        transform.Rotate(new Vector3(0f, 5f, 0f) * Time.deltaTime);
     }
 }
